0fb659357cbdfc4c5eeadb5feeb9c20fa4a0a62c
dumisanimaramba <dummie01@gmail.com>
2015-06-08 18:11:52 +0200

    initial commit

---
+++ WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+          name: "DefaultApiAction",
+          routeTemplate: "api/{controller}/{action}",
+          defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/PeopleController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class PeopleController : ApiController
+    {
+
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage list()
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person { Id = 1, FirstName = "Dumisani", LastName = "Maramba", Email = "dumi@test.com" });
+            people.Add(new Person { Id = 2, FirstName = "david", LastName = "Maramba", Email = "david@test.com" });
+            people.Add(new Person { Id = 3, FirstName = "King", LastName = "More", Email = "more@test.com" });
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, people);
+            return response;
+
+        }
+
+
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage add([FromBody]Person p)
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, p);
+            return response;
+
+        }
+    }
+}
